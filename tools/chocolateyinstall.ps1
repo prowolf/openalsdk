32e023af36333cc594893c002cf5dfa76085a53b
ProWolf <prowolfmailbox@gmail.com>
2019-05-07 16:03:51 -0400

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFileLocation   = Join-Path $toolsDir 'OpenAL11CoreSDK.zip'
+$exeFileLocation   = Join-Path $toolsDir 'OpenAL11CoreSDK.exe'
+$setupFileLocation = Join-Path $toolsDir 'setup-install.iss'
+$url        = 'https://www.openal.org/downloads/OpenAL11CoreSDK.zip'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  checksum      = '5FD3F70B0C5C6FCA825692EBA84BC0407C639CC14DC904C6A3154EDAE50CA8E1'
+  checksumType  = 'sha256'
+  checksum64    = $checksum
+  checksumType64= $checksumType
+
+  silentArgs    = "/s /f1`"$($setupFileLocation)`""
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyZipPackage @packageArgs -file $zipFileLocation
+Install-ChocolateyInstallPackage @packageArgs -file $exeFileLocation
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exeFileLocation   = Join-Path $toolsDir 'OpenAL11CoreSDK.exe'
+$setupFileLocation = Join-Path $toolsDir 'setup-uninstall.iss'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $exeFileLocation
+  silentArgs    = "/s /f1`"$($setupFileLocation)`""
+  validExitCodes= @(0)
+}
+
+$uninstalled = $false
+Uninstall-ChocolateyPackage @packageArgs
